0e0a9d01e6e37f18cc24b7f336c7231e647c2736
geoathan90 <70021082+geoathan90@users.noreply.github.com>
2026-09-13 10:09:34 +0000

    my first actually valuable query

--- sql/2_queries.sql
+++ sql/2_queries.sql
@@ -56,14 +56,39 @@
 -- GROUP BY w.ptype_code, p.name
 -- ORDER BY cell_hours DESC;
 
+-- SELECT
+--     g.latitude,
+--     g.longitude,
+--     COUNT(*) AS freezing_rain_cell_hours,
+--     ROUND(AVG(w.t2m_c), 2) AS avg_temp
+-- FROM weather_hourly w
+-- JOIN grid_cells g
+--     ON w.cell_id = g.cell_id
+-- WHERE w.ptype_code = 3
+-- GROUP BY g.cell_id
+-- ORDER BY freezing_rain_cell_hours DESC;
+
+
 SELECT
-    g.latitude,
-    g.longitude,
-    COUNT(*) AS freezing_rain_cell_hours,
-    ROUND(AVG(w.t2m_c), 2) AS avg_temp
-FROM weather_hourly w
-JOIN grid_cells g
-    ON w.cell_id = g.cell_id
-WHERE w.ptype_code = 3
-GROUP BY g.cell_id
-ORDER BY freezing_rain_cell_hours DESC;
+    t.time_utc,
+    gc.latitude,
+    gc.longitude,
+    ROUND(w.t2m_c, 2) AS temp,
+    ROUND(w.snowfall_swe_rate_mmh, 2) AS snowfall_rate,
+    ROUND(w.precip_rate_mmh, 2) AS precip_rate,
+    ROUND(w.gust_10m_ms, 2) AS gust_speed
+FROM weather_hourly AS w
+JOIN grid_cells AS gc
+    ON w.cell_id = gc.cell_id
+JOIN regions AS r
+    ON gc.region_id = r.region_id
+JOIN times AS t
+    ON w.time_id = t.time_id
+WHERE r.name = 'Larisa'
+  AND w.ptype_code = 6
+  AND w.snowfall_swe_rate_mmh > 3
+  AND gc.latitude BETWEEN 39.75 AND 40.00
+  AND gc.longitude BETWEEN 22.25 AND 22.50
+ORDER BY
+    t.time_utc,
+    w.snowfall_swe_rate_mmh DESC;
